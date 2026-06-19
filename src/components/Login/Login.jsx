8c1df503f6fd43526e36ae53366203fd03d98713
shantunu007 <chakmashantunu.web@gmail.com>
2026-06-19 20:19:04 +0600

    implement Google sign-in and display user profile

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,11 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
+import { useState } from "react";
 
 
 const Login = () => {
+
+    const [user, setUser] = useState(null);
 
     const provider = new GoogleAuthProvider();
 
@@ -11,7 +14,8 @@ const Login = () => {
 
         signInWithPopup(auth, provider)
             .then((result) => {
-                console.log(result);
+                console.log(result.user);
+                setUser(result.user);
             }).catch((error) => {
                 console.log(error);
             })
@@ -20,6 +24,13 @@ const Login = () => {
         <div>
             <h2>Please log in</h2>
             <button onClick={handleGoogleSignIn}>Sign in with google</button>
+
+            {
+                user && <div>
+                    <h3>{user.displayName}</h3>
+                    <h3>{user.email}</h3>
+                </div>
+            }
         </div>
     );
 };
